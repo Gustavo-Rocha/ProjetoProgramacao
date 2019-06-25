5da2aafbbfe5d13b41175079443b574474e59118
Gustavo Oliveira Rocha <gustavooliveirarocha@hotmail.com>
2019-06-25 19:13:05 -0300

    Aula 1 de exceptions ALURA

--- csharpexceptions-projetoInicial/ByteBank/Program.cs
+++ csharpexceptions-projetoInicial/ByteBank/Program.cs
@@ -15,52 +15,20 @@ namespace ByteBank
         }
         //Teste com a cadeia de chamada:
         //Metodo -> TestaDivisao -> Dividir
-        private static int Metodo()
+        private static void Metodo()
         {
-            int resultadoDivisao =TestaDivisao(0);
-
-            if (resultadoDivisao==-2)
-            {
-                return -224323;
-            }
             TestaDivisao(2);
-            return 0;
         }
 
-        private static int TestaDivisao(int divisor)
+        private static void TestaDivisao(int divisor)
         {
             int resultado = Dividir(10, divisor);
 
-            if (resultado==-2)
-            {
-                return -2;
-            }
-
-            if (resultado == -1)
-            {
-                Console.WriteLine("Não é possivel dividir por 0");
-            }
-            else
-            {
-                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
-            }
-
-            return 0;
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado)
         }
 
         private static int Dividir(int numero, int divisor)
         {
-
-            if(divisor==0)
-            {
-                return -1;
-            }
-
-            if (divisor > numero)
-            {
-                return -1;
-            }
-
             return numero / divisor;
         }
     }

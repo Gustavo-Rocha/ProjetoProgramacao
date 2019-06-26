4ff1c81c5f0e1c01ddc5e0e49a0fe336d658839e
Gustavo Oliveira Rocha <gustavooliveirarocha@hotmail.com>
2019-06-26 18:11:16 -0300

    Testando Try Catch e a Herança da Classe Exception para tratar varios tipos de exceção

--- csharpexceptions-projetoInicial/ByteBank/Program.cs
+++ csharpexceptions-projetoInicial/ByteBank/Program.cs
@@ -10,33 +10,48 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            Metodo();
+            try
+            {
+                Metodo();
+            }
+            catch(DivideByZeroException erro)
+            {
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Aconteceu um erro de divisão por 0");
+            }
+            catch(Exception erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Aconteceu um erro");
+            }
+
+            
             Console.ReadLine();
         }
         //Teste com a cadeia de chamada:
         //Metodo -> TestaDivisao -> Dividir
         private static void Metodo()
         {
-            TestaDivisao(2);
+            TestaDivisao(0);
         }
 
 
         private static void TestaDivisao(int divisor)
         {
-            try
-            {
+           
                 int resultado = Dividir(10, divisor);
-            }               
-            catch(DivideByZeroException)
-            {
-                Console.WriteLine("Não é possivel dividir por 0");
-            }
-
-            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+                Console.WriteLine("Resultado da divisão de 10 por " + divisor +"é"+ resultado);
+           
+             
         }
 
         private static int Dividir(int numero, int divisor)
         {
+            ContaCorrente conta = null;
+
+           Console.WriteLine(conta.Saldo);
+
             return numero / divisor;
         }
     }

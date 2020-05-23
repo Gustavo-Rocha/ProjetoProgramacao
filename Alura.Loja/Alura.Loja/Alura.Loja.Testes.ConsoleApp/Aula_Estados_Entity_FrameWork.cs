b276e620e1756f3bd56d4665370445aee29dc55b
Gustavo Rocha <gustavooliveirarocha@hotmail.com>
2020-05-23 17:52:15 -0300

    alura loja

---
+++ Alura.Loja/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Aula_Estados_Entity_FrameWork.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    partial class Aula_Estados_Entity_FrameWork
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new LojaContext())
+            {
+
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                var produtos = contexto.Produtos.ToList();
+
+
+                ExibirEntries(contexto.ChangeTracker.Entries());
+
+                var novoProduto = new Produto()
+                {
+                    Nome = "sabão em pó",
+                    Categoria = "Limpeza",
+                    Preco = 5.00
+
+                };
+
+
+                contexto.Produtos.Add(novoProduto);
+                ExibirEntries(contexto.ChangeTracker.Entries());
+                contexto.Produtos.Remove(novoProduto);
+
+                ExibirEntries(contexto.ChangeTracker.Entries());
+
+                // contexto.SaveChanges();
+
+                var entry = contexto.Entry(novoProduto);
+                Console.WriteLine("\n\n" + entry.Entity.ToString() + " - " + entry.State);
+
+                ExibirEntries(contexto.ChangeTracker.Entries());
+
+            }
+            Console.ReadLine();
+        }
+
+        private static void ExibirEntries(IEnumerable<EntityEntry> entries)
+        {
+            Console.WriteLine("\n ================== \n");
+
+            foreach (var e in entries)
+            {
+                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
+            }
+        }
+    }
+}
--- Alura.Loja/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,29 +1,23 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Alura.Loja.Testes.ConsoleApp
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-            GravarUsandoAdoNet();
+            
+            
+
         }
 
-        private static void GravarUsandoAdoNet()
-        {
-            Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;
-
-            using (var repo = new ProdutoDAO())
-            {
-                repo.Adicionar(p);
-            }
-        }
     }
 }
